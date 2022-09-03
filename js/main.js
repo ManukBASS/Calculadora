3939fd2b02f515405d30df23b87f91ef2fe73347
Manu Zarraga <manuzarraga@hotmail.com>
2022-09-03 15:01:18 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const displayValorAnterior = document.getElementById('valorAnterior');
+const displayValorActual = document.getElementById('valorActual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+
+const calculadora = new Calculadora();
+const display = new Display(displayValorAnterior, displayValorActual);
+
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.agregarNumero(boton.innerHTML)
+    })
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.computar(boton.value)
+    })
+});
